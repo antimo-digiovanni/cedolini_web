eb8b827a79d09f45472323c20741b6fe075fec46
Antimo Digiovanni <antimo.digiovanni@sanvincenzosrl.com>
2026-03-06 11:36:53 +0100

    Elimina file R2 quando si cancella un cedolino o CUD

--- portal/signals.py
+++ portal/signals.py
@@ -1,9 +1,9 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.signals import user_logged_in
 from django.contrib.auth.models import User
 
-from .models import Employee, AuditEvent
+from .models import Employee, AuditEvent, Payslip, Cud
 
 
 @receiver(post_save, sender=Employee)
@@ -33,3 +33,27 @@ def log_user_login(sender, request, user, **kwargs):
         import logging
         logger = logging.getLogger(__name__)
         logger.exception("Errore nella creazione di AuditEvent per user_logged_in")
+
+
+@receiver(post_delete, sender=Payslip)
+def delete_payslip_file_on_model_delete(sender, instance, **kwargs):
+    """Rimuove il PDF dal bucket quando il record Payslip viene eliminato."""
+    try:
+        if instance.pdf:
+            instance.pdf.delete(save=False)
+    except Exception:
+        import logging
+        logger = logging.getLogger(__name__)
+        logger.exception("Errore nella cancellazione file Payslip id=%s", getattr(instance, "id", None))
+
+
+@receiver(post_delete, sender=Cud)
+def delete_cud_file_on_model_delete(sender, instance, **kwargs):
+    """Rimuove il PDF CUD dal bucket quando il record Cud viene eliminato."""
+    try:
+        if instance.pdf:
+            instance.pdf.delete(save=False)
+    except Exception:
+        import logging
+        logger = logging.getLogger(__name__)
+        logger.exception("Errore nella cancellazione file CUD id=%s", getattr(instance, "id", None))
