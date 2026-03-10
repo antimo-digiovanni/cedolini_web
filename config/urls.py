888a8ec03bf0644a2a755f753dcbaa9b2644dd78
Antimo Digiovanni <antimo.digiovanni@sanvincenzosrl.com>
2026-03-10 22:42:28 +0100

    Add employee password reset flow

--- config/urls.py
+++ config/urls.py
@@ -31,6 +31,27 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 
+    # Recupero password
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset_form.html',
+        email_template_name='registration/password_reset_email.html',
+        subject_template_name='registration/password_reset_subject.txt',
+        success_url='/password-reset/done/'
+    ), name='password_reset'),
+
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'
+    ), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='registration/password_reset_confirm.html',
+        success_url='/reset/done/'
+    ), name='password_reset_confirm'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'
+    ), name='password_reset_complete'),
+
     # Cambio password (QUESTO RISOLVE L'ERRORE)
     path('password-change/', auth_views.PasswordChangeView.as_view(
         template_name='registration/password_change_form.html'
